f33c0abb7a6e311f7bbfec02c5e70a578b87f170
akshaygurme <42416798+akshaygurme@users.noreply.github.com>
2022-05-28 20:12:08 +0530

    node install

---
+++ script.js
@@ -0,0 +1,8 @@
+// Importing modules
+import { addToCart, cart, shippingCost } from './shoppingCart.js';
+
+console.log('Importing module');
+addToCart('Apple', 100);
+addToCart('Banana', 100);
+addToCart('Grapes', 100);
+console.log(cart);
---
+++ shoppingCart.js
@@ -0,0 +1,10 @@
+console.log('Exporting module');
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+export { cart, shippingCost };
